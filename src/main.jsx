6b7c6b4a24a7e4ffd1c61edc04de30a3f9a26bd4
Luiscef <enriquefranco2003@gmail.com>
2025-04-04 21:54:17 -0600

    correccion

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Asegúrate de que esto esté importado
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/pokedex"> 
-      <App />
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
